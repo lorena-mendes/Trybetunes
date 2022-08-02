942b8fa2c701f559d80b0d13a4dbeb1b5a00fd61
Lorena Mendes <lorena.lccma@gmail.com>
2022-08-02 19:38:15 -0300

    Iniciei requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import LoginForm from '../Components/LoginForm';
+// import PropTypes from 'prop-types';
+
+const minimumCharacter = 3;
 
 export default class Login extends Component {
+  state = {
+    userName: '',
+    buttonDisabled: true,
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+    const { userName } = this.setState;
+    if (userName.length >= minimumCharacter) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  }
+
   render() {
+    const { buttonDisabled } = this.setState;
+
     return (
-      <div data-testid="page-login">
-        Login
-      </div>
+      <section>
+        <div data-testid="page-login">
+          <LoginForm
+            buttonDisabled={ buttonDisabled }
+            handleChange={ this.handleChange }
+          />
+        </div>
+      </section>
     );
   }
 }
