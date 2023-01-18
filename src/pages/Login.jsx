e92dd0950198708c38da06d7f44e17445d14de36
lorena-mendes <lorena.lccma@gmail.com>
2023-01-17 22:02:00 -0300

    Finalizei css da página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
 import Loading from '../Components/Loading';
-import './login.css';
+import '../Css/login.css';
 
 const minimumCharacter = 3;
 
@@ -45,31 +45,33 @@ export default class Login extends Component {
     const { buttonDisabled, userName, loading } = this.state;
 
     return loading ? <Loading /> : (
-      <section data-testid="page-login" className="initial-page">
-        <h1>Trybetunes</h1>
-        <form>
-          <input
-            type="text"
-            data-testid="login-name-input"
-            placeholder="Digite seu nome"
-            name="userName"
-            className="login-name-input"
-            onChange={ this.handleChangeUserName }
-            value={ userName }
-          />
-        </form>
-        <button
-          className="button"
-          type="button"
-          data-testid="login-submit-button"
-          disabled={ buttonDisabled }
-          onClick={ this.handleClickLogin }
-          handleClick={ this.handleClickLogin }
-          loading={ loading }
-        >
-          Entrar
-        </button>
-      </section>
+      <div data-testid="page-login">
+        <section className="section-login">
+          <form className="form-login">
+            <h1>LOGIN</h1>
+            <input
+              type="text"
+              data-testid="login-name-input"
+              placeholder="Digite seu nome"
+              name="userName"
+              className="login-name-input"
+              onChange={ this.handleChangeUserName }
+              value={ userName }
+            />
+            <button
+              className="button"
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ buttonDisabled }
+              onClick={ this.handleClickLogin }
+              handleClick={ this.handleClickLogin }
+              loading={ loading }
+            >
+              Entrar
+            </button>
+          </form>
+        </section>
+      </div>
     );
   }
 }
